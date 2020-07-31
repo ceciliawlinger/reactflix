6a843e536f4a8c822fdfc06dd30b60f3e338434c
cecilia.wlinger <wlinger.cecilia@gmail.com>
2020-07-31 16:21:21 -0300

    Alteracoes Aula_02

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default function CadastroVideo() {
   return (
     <PageDefault>
-      <h1>Cadastro de Vídeo</h1>
+      <h1>Cadastro de Vídeos</h1>
       <Link to="/cadastro/categoria">Cadastrar Categoria</Link>
     </PageDefault>
   );
